5601cd73d9ea409064d70cfd22ccb25242794c82
v3gaj <juanvegaquesada@gmail.com>
2018-05-02 17:29:32 -0600

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,10 +29,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.precompile =  ['*.js', '*.css', '*.scss', '*min.css', '*.ttf', '*.woff', '*.woff2']
-  
-  config.public_file_server.enabled = true 
-
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
