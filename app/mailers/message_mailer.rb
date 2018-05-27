bed303188a47bf475fb4903323fa6a51d44fe386
v3gaj <juanvegaquesada@gmail.com>
2018-05-27 11:06:55 -0600

    Mailer avoid spam

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,12 +1,16 @@
+require 'digest/sha2'  
+
 class MessageMailer < ApplicationMailer
 
-	default from: "Your Mailer <webcontact@telescopehr.com>"
-  	default to: "Your Name <info@telescopehr.com>"
+	default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@telescopehr.com"
 
-  	def new_message(message)
-	    @message = message
-    
-    	mail subject: "Message from #{message.name}"
-  	end
+	default from: "Your Mailer <webcontact@telescopehr.com>"
+	default to: "Your Name <info@telescopehr.com>"
+
+	def new_message(message)
+    @message = message
+  
+  	mail subject: "Message from #{message.name}"
+	end
 
 end
