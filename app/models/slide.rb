77edd5b95b0d3c49abf86aa4a2bfaf9425e9bdd5
v3gaj <juanvegaquesada@gmail.com>
2018-05-31 12:46:57 -0600

    Image size

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -6,7 +6,7 @@ class Slide < ApplicationRecord
 	validates_presence_of :subtitle, message: :not_blank
 	validates_presence_of :background, message: :not_blank
 
-	has_attached_file :background, styles: { normal: '2250x1500#' }, convert_options: { normal: '-quality 50' }
+	has_attached_file :background, styles: { normal: '1680x1120#' }, convert_options: { normal: '-quality 50' }
 	validates_attachment_size :background, in: 0..3.megabytes, message: :exceeds_size
 	validates_attachment_content_type :background, content_type: /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, message: :incorrect_format
 
