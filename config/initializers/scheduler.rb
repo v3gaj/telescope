370fb6041c9658462523376b92d3f7c2f15de351
v3gaj <juanvegaquesada@gmail.com>
2018-07-12 10:47:18 -0600

    Schedule cron

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,18 +3,18 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
 
-scheduler.cron '40 10 * * *' do
+scheduler.cron '48 10 * * *' do
   User::user_profile_incomplete
 end
 
-scheduler.cron '42 10 * * *' do
+scheduler.cron '49 10 * * *' do
   User::user_profile_incomplete
 end
 
-scheduler.cron '43 10 * * *' do
+scheduler.cron '50 10 * * *' do
   User::user_profile_incomplete
 end
 
-scheduler.cron '44 10 * * *' do
+scheduler.cron '51 10 * * *' do
   User::user_profile_incomplete
 end
